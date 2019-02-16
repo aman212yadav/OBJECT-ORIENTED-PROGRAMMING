0fe3699b58de7e99e810ffc45fa7a020f4233b3e
aman212yadav <31543527+aman212yadav@users.noreply.github.com>
2019-02-16 15:52:15 +0530

    Create copyConstructor.cpp

---
+++ copyConstructor.cpp
@@ -0,0 +1,81 @@
+//copy constructor
+
+
+#include<bits/stdc++.h>
+using namespace std;
+class car{
+private:                // Access Modifier
+    // member variable
+    int price;
+    int modelNo;
+    char name[20];
+public:
+    //Default constructor
+      car(){};
+    //Parameterised constructor
+     car(int price,int modelNo,string in){
+         this->price=price;
+         this->modelNo=modelNo;
+         strcpy(name,in.c_str());
+     }
+     // Copy constructor
+     car(car &x){
+         cout<<"hello from copy constructor\n";
+        price= x.price;
+        modelNo=x.modelNo;
+        strcpy(name,x.name);
+     }
+    // Member function
+    void start()
+    {
+        cout<<"grr... starting the car "<<name<<endl;
+    }
+    // setters
+    // Using this pointer to distinguish local and memeber variable that are having same name
+    void setPrice(int price)
+    {
+        // Assuming price of car should be always be greater than equal 1000
+        if(price> 1000)
+            this->price=price;
+        else
+            this->price=1000;
+    }
+    void setModelNo(int modelNo)
+    {
+        this->modelNo=modelNo;
+    }
+    void setName(string s)
+    {
+        strcpy(name,s.c_str());
+    }
+    // getters
+    char * getName()
+    {
+        return name;
+    }
+    int getPrice()
+    {
+        return price;
+    }
+    int getModelNo()
+    {
+        return modelNo;
+    }
+    void print(){
+        cout<<getPrice()<<" "<<getModelNo()<<" "<<getName()<<"\n";
+        
+    }
+};
+int main(){
+ car c;
+ c.setName("BMW");
+ c.setPrice(500);
+ c.setModelNo(171210007);
+ car d(1000,171210007,"audi");
+ d.print();
+ car e(d);
+ e.print();
+ 
+ 
+
+}
